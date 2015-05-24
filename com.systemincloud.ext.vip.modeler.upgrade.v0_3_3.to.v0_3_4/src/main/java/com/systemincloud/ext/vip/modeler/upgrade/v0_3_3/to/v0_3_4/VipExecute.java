df664ae79e4020617b045ff96bb39279ece45314
mji <marek.jagielski@gmail.com>
2015-05-24 20:10:29 +0200

    v0_3_3.to.v0_3_4

---
+++ com.systemincloud.ext.vip.modeler.upgrade.v0_3_3.to.v0_3_4/src/main/java/com/systemincloud/ext/vip/modeler/upgrade/v0_3_3/to/v0_3_4/VipExecute.java
@@ -0,0 +1,28 @@
+package com.systemincloud.ext.vip.modeler.upgrade.v0_3_3.to.v0_3_4;
+
+import javax.xml.transform.TransformerException;
+
+import com.systemincloud.ext.vip.modeler.upgrade.common.AbstractVipExecute;
+import com.systemincloud.modeler.upgrade.common.IExtExecute;
+
+public class VipExecute extends AbstractVipExecute implements IExtExecute {
+	
+	@Override
+	public String executeOnFile(String xml) {
+		try {
+			xml = super.executeOnSic(xml);
+			xml = updateTaskVerTv(xml, "0.1.4");
+		} catch (TransformerException e) { }
+		return xml;
+	}
+	
+	@Override
+	public boolean executeOnRoot(String root) {
+		return super.executeOnRoot(root);
+	}
+	
+	@Override
+	public String executeOnPom(String pom) throws TransformerException{
+		return pom;
+	}
+}
