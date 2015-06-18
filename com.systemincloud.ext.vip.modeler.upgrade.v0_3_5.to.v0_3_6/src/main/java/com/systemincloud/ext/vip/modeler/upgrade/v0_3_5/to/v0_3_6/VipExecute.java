a137a32cbf67b543f2565ff010f63656bbb40bc7
mji <marek.jagielski@gmail.com>
2015-06-19 00:36:27 +0200

    to 0.3.6

---
+++ com.systemincloud.ext.vip.modeler.upgrade.v0_3_5.to.v0_3_6/src/main/java/com/systemincloud/ext/vip/modeler/upgrade/v0_3_5/to/v0_3_6/VipExecute.java
@@ -0,0 +1,48 @@
+package com.systemincloud.ext.vip.modeler.upgrade.v0_3_5.to.v0_3_6;
+
+import java.io.IOException;
+
+import javax.xml.transform.TransformerException;
+
+import org.apache.commons.io.IOUtils;
+
+import com.systemincloud.ext.vip.modeler.upgrade.common.AbstractVipExecute;
+import com.systemincloud.modeler.upgrade.common.AbstractExecute;
+import com.systemincloud.modeler.upgrade.common.IExtExecute;
+
+public class VipExecute extends AbstractVipExecute implements IExtExecute {
+	
+	@Override
+	public String executeOnFile(String xml) {
+		try {
+			xml = super.executeOnSic(xml);
+			xml = updateTaskVerColorMap     (xml, "0.1.4");
+			xml = updateTaskVerImageFromFile(xml, "0.1.5");
+			xml = updateTaskVerImageFromUrl (xml, "0.1.5");
+			xml = updateTaskVerImageToFile  (xml, "0.1.5");
+			xml = updateTaskVerTv           (xml, "0.1.6");
+			xml = updateTaskVerVideoFromFile(xml, "0.1.5");
+			xml = updateTaskVerVideoFromUrl (xml, "0.1.5");
+			xml = updateTaskVerVideoToFile  (xml, "0.1.6");
+			xml = updateTaskVerWebcam       (xml, "0.1.6");
+			
+			xml = addOnlyLocalAttribute(xml);
+			
+		} catch (TransformerException | IOException e) { }
+		return xml;
+	}
+	
+	public String addOnlyLocalAttribute(String xml) throws TransformerException, IOException {
+		return AbstractExecute.transform(xml, IOUtils.toString(VipExecute.class.getResourceAsStream("only-local.xsl"), "UTF-8"), null);
+	}
+	
+	@Override
+	public boolean executeOnRoot(String root) {
+		return super.executeOnRoot(root);
+	}
+	
+	@Override
+	public String executeOnPom(String pom) throws TransformerException{
+		return pom;
+	}
+}
